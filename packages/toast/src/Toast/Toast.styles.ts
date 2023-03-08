2515601fe828eadc4fc340146d79885d14fb1d9e
Shaneeza <shaneeza.ali@mongodb.com>
2023-03-08 12:14:32 -0500

    a little wobble

--- packages/toast/src/Toast/Toast.styles.ts
+++ packages/toast/src/Toast/Toast.styles.ts
@@ -1,16 +1,29 @@
 import { css, keyframes } from '@leafygreen-ui/emotion';
 
 const grow = keyframes`
-  0% {
-    scale: 0.3;
-  }
-  100% {
+  to {
     scale: 6;
   } 
+`;
+
+const shake = keyframes`
+  0% {
+    translate: 0% -3%;
+  } 
+  100% {
+    translate: 0% 3%;
+  }
 `;
 
 export const toastStyles = css`
   scale: 0.3;
   transform-origin: top;
-  animation: ${grow} 30000ms 1s cubic-bezier(0, 0, 1, 0) infinite;
+  translate: 0% 0%;
+  animation-name: ${grow}, ${shake};
+  animation-duration: 30000ms, 2s;
+  animation-timing-function: cubic-bezier(0, 0, 1, 0), ease-in-out;
+  animation-delay: 1s, 1s;
+  animation-iteration-count: infinite;
+  animation-direction: normal, alternate;
+  animation-fill-mode: none, both;
 `;
