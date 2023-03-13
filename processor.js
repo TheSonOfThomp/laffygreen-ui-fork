584e6509fd2ad0e8bb643758e18e4bd8ab056687
Adam Thompson <adam.thompson@mongodb.com>
2023-03-13 09:37:12 -0400

    Updates

--- processor.js
+++
@@ -1,9 +0,0 @@
-// processor.js
-export class WorkletProcessor extends AudioWorkletProcessor {
-  process(inputs, outputs, parameters) {
-    // Do something with the data, e.g. convert it to WAV
-    return true;
-  }
-}
-
-registerProcessor('worklet-processor', WorkletProcessor);
